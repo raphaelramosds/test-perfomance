39c50bc0b88cd58d0ec1739811eaf897d321e3d6
raphaelramosds <raphael.opensource@gmail.com>
2022-03-19 20:27:00 -0300

    feat: cluster module

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const { json } = require("express");
 const express = require("express");
 const router = require("./routes");
+
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
 const app = express();
 
 app.use(json());
 app.use(router);
 
-app.listen(3333);
+// dividir carga de processos
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    // criar workers (processos filhos)
+    cluster.fork();
+  } 
+} else {
+  app.listen(3333);
+}
